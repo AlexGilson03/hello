a2c5d3daaf1d8da270001929a468227352834e8d
Alex <AGilson3@huskers.unl.edu>
2021-08-26 19:10:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Alex Gilson
+ * Date: 8/26/2021
+ * 
+ * Hack 1.0 program 
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char** argv){
+     
+     printf("Alex\n");
+     return 0;
+     
+ }
